914fe5073e06bdc93c80818b101157c5359ba189
Shormi Ghosh <quanta5419@gmail.com>
2026-02-16 05:56:04 +0600

    Add CI workflow and make mvnw executable

---
+++ src/test/java/com/example/student_management/service/CustomUserDetailsServiceTest.java
@@ -0,0 +1,15 @@
+package com.example.student_management.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CustomUserDetailsServiceTest {
+
+    @Test
+    void loadUserByUsername() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
